f45648213e5aa75edbd24b536914a7e4d4081af0
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-18 15:09:50 +0530

    message

--- 08-install.sh
+++ 08-install.sh
@@ -6,7 +6,18 @@ if [ $USERID -ne 0 ]
 
 then 
     echo " Erroe:: please run with root access"
+    exit 1
 else
     echo " you are having root please proceed"
+fi
+
+
+yum install nginx -y
+
+if [ $? -ne 0 ]
+then
+    echo " installation is failure"
+else
+    echo "insatlltion is success"
 fi
 
