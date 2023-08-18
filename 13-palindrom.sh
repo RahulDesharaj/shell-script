7a38d16982e4b358d5209ff0eb831e65a3f27135
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-18 18:26:05 +0530

    message

--- 13-palindrom.sh
+++ 13-palindrom.sh
@@ -10,6 +10,10 @@ INPUT=${#STRINGNAME}
 
 reverse=""
 
+R="\e[\32m"
+N="\e[\0m"
+Y="\e[\33m"
+
 echo " enter string name is : $STRINGNAME"
 
 for (( i=$INPUT-1; i>=0; i-- ))
@@ -21,10 +25,10 @@ done
 if [ $STRINGNAME == $reverse ]
 
 then
-    echo " enter string is a palindrom"
+    echo -e " $R enter string is a palindrom $N"
     
 else
-    echo "enter string is not an panlindrom"
+    echo -e  " $Y enter string is not an panlindrom $N"
 fi
 
 
