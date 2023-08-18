cd1caf1d8bcf069d52a96c145e6aade829fd2672
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-18 18:18:15 +0530

    message

--- 13-palindrom.sh
+++ 13-palindrom.sh
@@ -17,12 +17,13 @@ do
 	reverse="$reverse${input:$i:1}"
 done
 
-if [ $INPUT == $reverse ]
 
- then
+if [ $input == $reverse ]
+
+then
     echo " enter string is a palindrom"
     
- else
+else
     echo "enter string is not an panlindrom"
 fi
 
