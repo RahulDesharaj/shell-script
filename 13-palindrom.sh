1be51e5019cdcdef965e232a69cabaebfa2e8dfd
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-18 17:37:20 +0530

    message

--- 13-palindrom.sh
+++ 13-palindrom.sh
@@ -6,5 +6,18 @@ STRINGNAME=$()
 
 read -s STRINGNAME
 
+INPUT=$(input)
+
+reverse=""
+
 echo " enter string name is : $STRINGNAME"
+
+if [ $INPUT == $reverse]
+
+then
+    echo " enter string is palindrom"
+    exit 1
+else
+    echo "enter string is not an panlindrom"
+fi
 
