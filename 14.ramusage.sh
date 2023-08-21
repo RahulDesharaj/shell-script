cdb76945e950e6cf93f8cbc8ef4bc25dae78031d
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-21 15:42:45 +0530

    updated

--- 14.ramusage.sh
+++ 14.ramusage.sh
@@ -12,14 +12,13 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-RAM_USAGE=$(free -m |grep mem | awk '/Mem/{print $3})
+RAM_USAGE=$(free -m | grep mem | awk '{print $3}')
 
 RAM_USAGE_THRESHOLD=1
 
-message=""
 
+message="system is runnig out of RAM $RAM_USAGE"
 
-
-
-
-
+if [ $RAM_USAGE -gt $RAM_USAGE_THRESHOLD ];then
+    echo -e "$Y $message: $RAM_USAGE"
+fi    
