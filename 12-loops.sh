4f3c592d286f9630f3205442ea55c95f4222cd0a
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-21 13:52:01 +0530

    updated

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 stud=("abc" "sdf" "dfg")
-echo "Student details are : ${stud[0] ,stud[1]}"
+echo "Student details are : ${stud[0],stud[1]}"
+
+
+PERSONS=("Ramesh" "Suresh" "Sachin")
+# inside array we always refer values with index and it starts from 0
+
+echo "First Person: ${PERSONS[0]}"
