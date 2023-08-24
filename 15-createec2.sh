c3d3a168939c91f318914fc95a72775dab9766a9
RahulDesharaj <rahuldesharaj92@gmail.com>
2023-08-24 20:35:43 +0530

    updated outputs

--- 15-createec2.sh
+++ 15-createec2.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
-NAMES=("mongodb" "web" "catalouge" "mysql" "redis" "rabbitmq" "shipping" "payment" "dispatch" "user" "cart")
-
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-04c41dde9b1452d4c
 DOMAIN_NAME=desharaju.online
 
-
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
 for i in "${NAMES[@]}"
-
-do
-
-    echo "NAMES : $i"
-
-    
-
+do  
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
-
-        INSTANCETYPE="t3.medium"
+        INSTANCE_TYPE="t3.medium"
     else
-        INSTANCETYPE="t2.micro"
-    fi     
-
+        INSTANCE_TYPE="t2.micro"
+    fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS" 
-
+    echo "created $i instance: $IP_ADDRESS"
 
     aws route53 change-resource-record-sets --hosted-zone-id Z06677163UV3U8W515927 --change-batch '
     {
@@ -41,6 +31,5 @@ do
                             "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
                         }}]
     }
-    '  
-
+    '
 done
